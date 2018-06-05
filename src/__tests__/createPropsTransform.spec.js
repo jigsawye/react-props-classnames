0839f208d4dec5cb05ce5632ed04c10f688c04c0
jigsawye <jigsaw.ye@gmail.com>
2018-06-05 23:11:22 +0800

    Add getDisplayName for wrappered component

--- src/__tests__/createPropsTransform.spec.js
+++ src/__tests__/createPropsTransform.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, mount } from 'enzyme';
 import styled from 'styled-components';
 
 import createPropsTransform from '../createPropsTransform';
@@ -48,5 +48,37 @@ describe('createPropsTransform', () => {
     expect(component.prop('class')).toMatch(/test-size-lg/);
     expect(component.prop('class')).not.toMatch(/test-light/);
     expect(component.prop('class')).not.toMatch(/test-type-primary/);
+  });
+
+  it('should render component name as `PropsTransformer(DisplayName)`', () => {
+    const propsTansform = createPropsTransform({
+      prefix: 'test',
+      props: ['disabled', 'size'],
+    });
+
+    const StyledComp = styled.div`
+      background-color: #fff;
+    `;
+    const TransformedStyleComp = propsTansform(StyledComp);
+    const StyledComponent = mount(<TransformedStyleComp />);
+
+    const FunctionalComp = () => <div />;
+    const TransformedFunctionalComp = propsTansform(FunctionalComp);
+    const FunctionalComponent = mount(<TransformedFunctionalComp />);
+
+    const UnknownComp = () => () => <div />;
+    const TransformedUnknownComp = propsTansform(UnknownComp());
+    const UnknownComponent = mount(<TransformedUnknownComp />);
+
+    expect(StyledComponent).toMatchSnapshot();
+    expect(StyledComponent.name()).toEqual('PropsTransformer(styled.div)');
+
+    expect(FunctionalComponent).toMatchSnapshot();
+    expect(FunctionalComponent.name()).toEqual(
+      'PropsTransformer(FunctionalComp)'
+    );
+
+    expect(UnknownComponent).toMatchSnapshot();
+    expect(UnknownComponent.name()).toEqual('PropsTransformer(Unknown)');
   });
 });
